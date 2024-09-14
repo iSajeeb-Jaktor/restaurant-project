11e44f888d8e8942aac9ab1b9cd9891e0dbd0684
Md-Masum-Billah-official <omit.official.bd@gmail.com>
2024-09-14 20:19:42 +0600

    added banner

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar fixed z-10 bg-opacity-5 max-w-screen-xl bg-base-100">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
